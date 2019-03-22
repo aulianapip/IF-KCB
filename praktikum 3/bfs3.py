0421f97bf9308bea01e79926ea38e95a14314110
aulianapip <naufalapip45@gmail.com>
2019-03-22 09:25:42 +0700

    tambah file bfs3.py

---
+++ praktikum 3/bfs3.py
@@ -0,0 +1,26 @@
+vertexList = ['0', '1', '2', '3', '4', '5', '6']
+edgeList = [(0,1), (0,2), (1,0), (1,3), (2,0)
+            , (2,4), (2,5), (3,1), (4,2)
+            , (4,6), (5,2), (6,4)]
+graphs = (vertexList, edgeList)
+
+def bfs(graph, start):
+    vertexList, edgeList = graph
+    visitedList = []
+    queue = [start]
+    adjacencyList = [[] for vertex in vertexList]
+
+    
+    for edge in edgeList:
+        adjacencyList[edge[0]].append(edge[1])
+
+    # bfs
+    while queue:
+        current = queue.pop()
+        for neighbor in adjacencyList[current]:
+            if not neighbor in visitedList:
+                queue.insert(0,neighbor)
+        visitedList.append(current)
+    return visitedList
+
+print(bfs(graphs, 0))
